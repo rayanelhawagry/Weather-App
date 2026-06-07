3c7d9e1ab802da4d9f214b89610d14ad175496f6
Rayan Elhawagry <se.mycareer@gmail.com>
2026-06-08 01:58:24 +0300

    Clean & organize my code

--- js/main.js
+++ js/main.js
@@ -2,11 +2,8 @@
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const weatherCards = document.querySelector('.weather-cards');
-const firstDayCard = document.querySelector('.first-day');
-const secondDayCard = document.querySelector('.second-day');
-const thirdDayCard = document.querySelector('.third-day');
-let currentLocation = "Cairo"; // Make "Geolocation API" as a default...
-let locationDetails = [];
+let currentLocation = "Cairo";
+let locationDetails = null;
 let today;
 let secondDay;
 let thirdDay;
@@ -31,25 +28,31 @@ input.addEventListener("keydown", function (e) {
 
 // Fetch data from API
 const fetchData = async () => {
-    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=a5255ab1b8c04f1e9eb205856262905&q=${currentLocation}&days=7`);
-    let data = await response.json();
-    locationDetails = data;
+    try {
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=a5255ab1b8c04f1e9eb205856262905&q=${currentLocation}&days=7`);
+        let data = await response.json();
+        locationDetails = data;
 
-    // Date Formatter
-    let dateFormatter = new Intl.DateTimeFormat("en-GB", { dateStyle: "full" });
-    today = new Date(locationDetails.location.localtime);
-    secondDay = new Date(locationDetails.forecast.forecastday[1].date);
-    thirdDay = new Date(locationDetails.forecast.forecastday[2].date);
 
-    today = dateFormatter.format(today);
-    secondDay = dateFormatter.format(secondDay);
-    thirdDay = dateFormatter.format(thirdDay);
+        // Date Formatter
+        let dateFormatter = new Intl.DateTimeFormat("en-GB", { dateStyle: "full" });
+        today = new Date(locationDetails.location.localtime);
+        secondDay = new Date(locationDetails.forecast.forecastday[1].date);
+        thirdDay = new Date(locationDetails.forecast.forecastday[2].date);
 
-    arrToday = today.split(",");
-    arrSecondDay = secondDay.split(",");
-    arrThirdDay = thirdDay.split(",");
+        today = dateFormatter.format(today);
+        secondDay = dateFormatter.format(secondDay);
+        thirdDay = dateFormatter.format(thirdDay);
 
-    displayData();
+        arrToday = today.split(",");
+        arrSecondDay = secondDay.split(",");
+        arrThirdDay = thirdDay.split(",");
+
+
+        displayData();
+    } catch (error) {
+        console.error(error);
+    };
 };
 
 fetchData();
@@ -57,6 +60,9 @@ fetchData();
 
 // Display data
 const displayData = () => {
+    input.value = "";
+
+    // Render in HTML
     weatherCards.innerHTML = `
     <!-------------------- First Day Card -------------------->
     <div class="outer">
